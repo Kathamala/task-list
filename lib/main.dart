07f2137b0176945bbcf035bf3e112a115b49ebbb
unknown <thiago.oliveira.709@imd.local>
2022-10-24 14:01:14 -0300

    Implements task list app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main(List<String> args) {
   runApp(const MaterialApp(
@@ -18,8 +19,161 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List _todoList = [];
+  Map<String, dynamic> _lastRemoved = {};
+  int _lastRemovedPos = 0;
+  final TextEditingController _todoController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _readData().then((data) {
+      setState(() {
+        _todoList = json.decode(data);
+      });
+    });
+  }
+
+  Future<File> _getFile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File("${directory.path}/data.json");
+  }
+
+  Future<File> _saveData() async {
+    String data = json.encode(_todoList);
+    final file = await _getFile();
+    return file.writeAsString(data);
+  }
+
+  Future<String> _readData() async {
+    final file = await _getFile();
+    return file.readAsString();
+  }
+
+  void addTodo() {
+    setState(() {
+      Map<String, dynamic> newTodo = {};
+      newTodo["title"] = _todoController.text;
+      _todoController.text = "";
+      newTodo["ok"] = false;
+      _todoList.add(newTodo);
+      _saveData();
+    });
+  }
+
+  void checkTodo(index, c) {
+    setState(() {
+      _todoList[index]["ok"] = c;
+      _saveData();
+    });
+  }
+
+  Future<void> _refresh() async {
+    await Future.delayed(const Duration(seconds: 1));
+
+    setState(() {
+      _todoList.sort((a, b) {
+        if (a["ok"] && !b["ok"]) {
+          return 1;
+        } else if (!a["ok"] && b["ok"]) {
+          return -1;
+        } else {
+          return 0;
+        }
+      });
+
+      _saveData();
+    });
+  }
+
+  Widget buildItem(context, index) {
+    return Dismissible(
+        key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+        background: Container(
+          color: Colors.red,
+          child: const Align(
+            alignment: Alignment(-0.9, 0.0),
+            child: Icon(
+              Icons.delete,
+              color: Colors.white,
+            ),
+          ),
+        ),
+        direction: DismissDirection.startToEnd,
+        child: CheckboxListTile(
+          title: Text(_todoList[index]["title"]),
+          value: _todoList[index]["ok"],
+          secondary: CircleAvatar(
+            child: Icon(_todoList[index]["ok"] ? Icons.check : Icons.error),
+          ),
+          onChanged: (value) {
+            checkTodo(index, value);
+          },
+        ),
+        onDismissed: (direction) {
+          setState(() {
+            _lastRemoved = Map.from(_todoList[index]);
+            _lastRemovedPos = index;
+            _todoList.removeAt(index);
+            _saveData();
+
+            final snack = SnackBar(
+              content: Text("Tarefa ${_lastRemoved["title"]} removida"),
+              action: SnackBarAction(
+                label: "Desfazer",
+                onPressed: () {
+                  setState(() {
+                    _todoList.insert(_lastRemovedPos, _lastRemoved);
+                    _saveData();
+                  });
+                },
+              ),
+              duration: const Duration(seconds: 2),
+            );
+
+            ScaffoldMessenger.of(context).removeCurrentSnackBar();
+            ScaffoldMessenger.of(context).showSnackBar(snack);
+          });
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    Scaffold screen = Scaffold(
+      appBar: AppBar(
+        title: const Text("Lista de tarefas"),
+        backgroundColor: Colors.blueAccent,
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.fromLTRB(17, 1, 7, 1),
+            child: Row(
+              children: [
+                Expanded(
+                    child: TextField(
+                  controller: _todoController,
+                  decoration: const InputDecoration(
+                      labelText: "Nova Tarefa",
+                      labelStyle: TextStyle(color: Colors.blueAccent)),
+                )),
+                ElevatedButton(onPressed: addTodo, child: const Text("ADD"))
+              ],
+            ),
+          ),
+          Expanded(
+              child: RefreshIndicator(
+            onRefresh: _refresh,
+            child: ListView.builder(
+                padding: const EdgeInsets.only(top: 10),
+                itemCount: _todoList.length,
+                itemBuilder: buildItem),
+          ))
+        ],
+      ),
+    );
+
+    return screen;
   }
 }
